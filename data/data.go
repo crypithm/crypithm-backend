f173e9478daabc12113db44e1803dca4323a608d
rootxdwt <65634206+rootxdwt@users.noreply.github.com>
2022-06-22 14:01:19 +0900

    updated folder

--- data/data.go
+++ data/data.go
@@ -26,6 +26,11 @@ type Filedata struct {
 	Dir  string
 }
 
+type AppendedFileData struct {
+	Folders []interface{}
+	Message string
+}
+
 type Folderdata struct {
 	Id    string
 	Name  string
@@ -51,25 +56,39 @@ func Datahandle(w http.ResponseWriter, r *http.Request) {
 			message, _ := json.Marshal(Defaultresp{"Error"})
 			fmt.Fprintf(w, string(message))
 		} else {
-			var uid string
-			rows.Scan(&uid)
-			fileRows, _ := db.Query("SELECT size,name,id, directory FROM files WHERE userid=?", uid)
-			var returnData Fileresponse
-			for fileRows.Next() {
-				var fileJson Filedata
-				fileRows.Scan(&fileJson.Size, &fileJson.Name, &fileJson.Id, &fileJson.Dir)
-				returnData.Files = append(returnData.Files, fileJson)
+			if r.FormValue("action") == "getOnlyFolder" {
+				folderRows, _ := db.Query("SELECT name,id, date, parent FROM folder WHERE userid=?", uid)
+				//Folderdata
+				var folders AppendedFileData
+				for folderRows.Next() {
+					var folderjson Folderdata
+					folderRows.Scan(&folderjson.Name, &folderjson.Id, &folderjson.Date, &folderjson.Index)
+					folders.Folders = append(folders.Folders, folderjson)
+				}
+				folders.Message = "Success"
+				returnJSONarr, _ := json.Marshal(folders)
+				fmt.Fprintf(w, string(returnJSONarr))
+			} else {
+				var uid string
+				rows.Scan(&uid)
+				fileRows, _ := db.Query("SELECT size,name,id, directory FROM files WHERE userid=?", uid)
+				var returnData Fileresponse
+				for fileRows.Next() {
+					var fileJson Filedata
+					fileRows.Scan(&fileJson.Size, &fileJson.Name, &fileJson.Id, &fileJson.Dir)
+					returnData.Files = append(returnData.Files, fileJson)
+				}
+				folderRows, _ := db.Query("SELECT name,id, date, parent FROM folder WHERE userid=?", uid)
+				//Folderdata
+				for folderRows.Next() {
+					var folderjson Folderdata
+					folderRows.Scan(&folderjson.Name, &folderjson.Id, &folderjson.Date, &folderjson.Index)
+					returnData.Folders = append(returnData.Folders, folderjson)
+				}
+				returnData.Message = "Success"
+				returnJSONarr, _ := json.Marshal(returnData)
+				fmt.Fprintf(w, string(returnJSONarr))
 			}
-			folderRows, _ := db.Query("SELECT name,id, date, parent FROM folder WHERE userid=?", uid)
-			//Folderdata
-			for folderRows.Next() {
-				var folderjson Folderdata
-				folderRows.Scan(&folderjson.Name, &folderjson.Id, &folderjson.Date, &folderjson.Index)
-				returnData.Folders = append(returnData.Folders, folderjson)
-			}
-			returnData.Message = "Success"
-			returnJSONarr, _ := json.Marshal(returnData)
-			fmt.Fprintf(w, string(returnJSONarr))
 		}
 	}
 }
