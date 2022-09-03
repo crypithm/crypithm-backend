a4e82e5b2d660e30e94d1c529231bb5cad372f1b
rootxdwt <65634206+rootxdwt@users.noreply.github.com>
2022-09-03 21:02:40 +0900

    Update preupload.go

--- preupload/preupload.go
+++ preupload/preupload.go
@@ -86,11 +86,8 @@ func Prehandle(w http.ResponseWriter, r *http.Request) {
 		var uid string
 		rows.Scan(&uid)
 
-		if e != nil {
-			message, _ = json.Marshal(Response{"saveError"})
-			fmt.Fprintf(w, string(message))
-			return
-		}
+		fileName := randstring(16)
+
 		defer file.Close()
 		_, e = db.Exec("INSERT INTO files (size, name,blobkey,id,directory,userid,savedname) values (?,?,?,?,?,?,?)", recievedVals[0], recievedVals[1], recievedVals[2], recievedVals[3], recievedVals[4], uid, fileName)
 		if e != nil {
